3406fc55222d043e5da8a8021c3a160da33e059c
Abheet Gera <abheet.gera01@gmail.com>
2022-01-13 16:52:30 +0530

    Update Weird-Stock.cpp

--- Weird-Stock.cpp
+++ Weird-Stock.cpp
@@ -36,6 +36,17 @@ int main() {
     
     cout<<days;
 
+	
+	//sol2
+	/*
+	    int res = 0;
+	    while(n < m){
+		m = m & 1 ? m + 1 : m / 2;
+		res++;
+	    }
+	    cout << res + (n - m) << endl;
+	*/
+	
 	return 0;
 }
 
